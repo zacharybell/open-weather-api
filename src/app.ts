6e6473d641a968c61b6c27f227410dd77b3d1a2b
Zachary Bell <zacharybell@live.com>
2021-10-21 22:34:02 -0500

    Add proxied call to openweather one call endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
+require('dotenv').config();
+
 import express from 'express';
+import { getOneCall } from './service/open-weather';
 
 const app = express();
 
-app.get('/openweather', (req, res) => {
-    res.send('HELLO WORLD');
+app.get('/weather', async (req, res, next) => {
+    try {
+        const weather = await getOneCall(req.query);
+        res.send(weather.data);
+    } catch (error) {
+        next(error);
+    }
 });
 
 app.listen(9000, () => {
---
+++ src/service/open-weather.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const openApiClient = axios.create({
+    baseURL: "https://api.openweathermap.org/data/2.5",
+    params: {
+        appid: process.env.OPEN_WEATHER_API_KEY
+    }
+});
+
+export async function getOneCall(params: any) {
+    return openApiClient.get("/onecall", { params });
+}
