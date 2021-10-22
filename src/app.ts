eae8322379345208db60f4107c4b89cdf6fa514c
Zachary Bell <zacharybell@live.com>
2021-10-22 18:10:10 -0500

    Add express-async-errors to prevent need to call next for error handling

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 require('dotenv').config();
+require('express-async-errors');
 
 import express from 'express';
 import { getOneCall } from './service/open-weather';
@@ -6,13 +7,9 @@ import { getOneCall } from './service/open-weather';
 const app = express();
 const PORT = 9000;
 
-app.get('/weather', async (req, res, next) => {
-    try {
-        const weather = await getOneCall(req.query);
-        res.send(weather.data);
-    } catch (error) {
-        next(error);
-    }
+app.get('/weather', async (req, res) => {
+    const weather = await getOneCall(req.query);
+    res.send(weather.data);
 });
 
 app.listen(PORT, () => {
