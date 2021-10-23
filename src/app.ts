19799d168e36ca332eb26ce6870b5146e0b8ba1f
Zachary Bell <zacharybell@live.com>
2021-10-22 20:50:22 -0500

    Add rate limiting and caching

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,29 @@ require('dotenv').config();
 require('express-async-errors');
 
 import express from 'express';
+import rateLimit from 'express-rate-limit';
+import apicache from 'apicache';
+
 import { errorHandler } from './middleware/error-handler';
 import { getOneCall } from './service/open-weather';
 
 const app = express();
 const PORT = 9000;
+
+app.set('trust proxy', true);
+app.set('x-powered-by', false);
+
+// caching
+const cache = apicache.middleware;
+app.use(cache('5 minutes'));
+
+// rate limiting
+const limiter = rateLimit({
+    message: undefined,
+    windowMs: 10 * 60 * 1000, // 10 Mins
+    max: 20
+});
+app.use(limiter);
 
 app.get('/weather', async (req, res) => {
     const weather = await getOneCall(req.query);
