19f9f58a5b3dc190d40c63fa941d0d19511b2a17
Zachary Bell <zacharybell@live.com>
2021-12-21 01:24:07 -0500

    Add AWS HTTP Gateway with integration to lambda

--- main.tf
+++ main.tf
@@ -46,3 +46,62 @@ resource "aws_lambda_function" "onecall" {
   handler       = "app.handler"
   runtime       = "nodejs14.x"
 }
+
+resource "aws_apigatewayv2_api" "main" {
+  name          = "${local.prefix}-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_cloudwatch_log_group" "api_gw" {
+  name = "/aws/api_gw/${aws_apigatewayv2_api.main.name}"
+
+  retention_in_days = 5
+}
+
+resource "aws_apigatewayv2_stage" "v1" {
+  api_id = aws_apigatewayv2_api.main.id
+
+  name        = "v1"
+  auto_deploy = true
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.api_gw.arn
+
+    format = jsonencode({
+      requestId               = "$context.requestId"
+      sourceIp                = "$context.identity.sourceIp"
+      requestTime             = "$context.requestTime"
+      protocol                = "$context.protocol"
+      httpMethod              = "$context.httpMethod"
+      resourcePath            = "$context.resourcePath"
+      routeKey                = "$context.routeKey"
+      status                  = "$context.status"
+      responseLength          = "$context.responseLength"
+      integrationErrorMessage = "$context.integrationErrorMessage"
+      }
+    )
+  }
+}
+
+resource "aws_apigatewayv2_integration" "onecall" {
+  api_id = aws_apigatewayv2_api.main.id
+
+  integration_uri    = aws_lambda_function.onecall.invoke_arn
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+}
+
+resource "aws_apigatewayv2_route" "onecall" {
+  api_id = aws_apigatewayv2_api.main.id
+
+  route_key = "GET /weather"
+  target    = "integrations/${aws_apigatewayv2_integration.onecall.id}"
+}
+
+resource "aws_lambda_permission" "onecall" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.onecall.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.main.execution_arn}/*/*"
+}
