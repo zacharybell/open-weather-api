bd991dfe348bbe01094ea03a3d066db6881f4642
Zachary Bell <zacharybell@live.com>
2021-12-20 19:38:48 -0500

    Add lambda resource

--- main.tf
+++ main.tf
@@ -17,3 +17,32 @@ terraform {
 provider "aws" {
   region = "us-east-2"
 }
+
+locals {
+  prefix = "weather-${terraform.workspace}"
+}
+
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "${local.prefix}-lambda-exec-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_lambda_function" "onecall" {
+  filename      = "dist/bundle.zip"
+  function_name = "${local.prefix}-onecall"
+  role          = aws_iam_role.lambda_exec_role.arn
+  handler       = "app.handler"
+  runtime       = "nodejs14.x"
+}
